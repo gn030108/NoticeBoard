83e6bf8a3bcab767411379532ca8a3a018c84eae
power30111_HOME <power30111@naver.com>
2024-01-14 16:31:29 +0900

    회원가입,로그인기능, 토큰기능 추가

--- springBoard/src/main/java/backend/springBoard/Service/MemberService.java
+++ springBoard/src/main/java/backend/springBoard/Service/MemberService.java
@@ -10,8 +10,14 @@ import org.springframework.stereotype.Service;
 public class MemberService {
 
     private final MemberRepository memberRepository;
+    private final TokenProvider tokenProvider;
 
-    //회원가입
+    /**
+     * 회원가입
+     * @param name 회원 이름
+     * @param memberId 회원의 아이디
+     * @param pw 회원의 비밀번호
+     */
     public void createMember(String name, String memberId, String pw){
         Member newMember = Member.builder()
                 .name(name)
@@ -21,12 +27,29 @@ public class MemberService {
         memberRepository.save(newMember);
     }
     //로그인 -> 로그인시 uuid토큰을 반환하도록.
-    public void login(String memberId, String pw){
+
+    /**
+     * 회원의 일반 로그인
+     * @param memberId 회원 아이디
+     * @param pw 회원 비밀번호
+     * @return 일반 로그인 성공시 token 키값 반환. 실패시 null
+     */
+    public String login(String memberId, String pw){
         String byPassword = memberRepository.findByPassword(memberId);
         if(byPassword.equals(pw)){
             //DB저장된 pw와 입력된 pw가 같을경우 로그인성공.
-
+            return tokenProvider.createToken(memberId);
         }
+        return "null";
+    }
+
+    /**
+     * 토큰을 주면 로그인 유효시간이 남았는지 검사하는 메서드.
+     * @param token 토큰 키값
+     * @return 인증성공시 true, 예외발생시 false;
+     */
+    public Boolean tokenLogin(String token){
+        return tokenProvider.validToken(token);
     }
 
 
---
+++ springBoard/src/main/java/backend/springBoard/Service/TokenProvider.java
@@ -0,0 +1,60 @@
+package backend.springBoard.Service;
+
+import backend.springBoard.Domain.Token;
+
+import java.time.LocalTime;
+import java.time.temporal.ChronoField;
+import java.util.HashMap;
+import java.util.UUID;
+
+public class TokenProvider {
+
+    long duration = 30;
+
+    HashMap<String,Token> tokenStorage = new HashMap<>();
+
+    /**
+     * 토큰 생성 메서드. uuid, 현재시간, memberId가 들어있는 Token을 생성후 hashMap에서 관리한다.
+     * @param memberId 회원의 아이디
+     * @return String 형태의 tokenUUID
+     */
+    public String createToken(String memberId){
+
+        String uuid = memberId+UUID.randomUUID().toString();
+
+        LocalTime now = LocalTime.now();
+        long second = now.getLong(ChronoField.SECOND_OF_DAY);
+
+        Token token = Token.builder()
+                .memberId(memberId)
+                .token(uuid)
+                .memberId(memberId)
+                .build();
+        tokenStorage.put(uuid,token);
+        return uuid;
+
+    }
+
+    /**
+     * 토큰검사. tokenKey를 주면 유효시간을 검사한다. 기본적으로 하루시간중 몇분이나 지났는지를 따지기때문에
+     * 여기서 다음날로 넘어간다거나 하면 오류가 생길수도..??
+     * @param tokenKey 해당하는 토큰의 키값
+     * @return 유효시간이 있다면 true, 아닐경우 false;
+     */
+    public Boolean validToken(String tokenKey){
+
+        if(tokenStorage.containsKey(tokenKey)){
+            //해당하는 키가 있는경우
+            Token token = tokenStorage.get(tokenKey);
+
+            LocalTime now = LocalTime.now();
+            long nowSecond = now.getLong(ChronoField.SECOND_OF_DAY);
+
+            //유효시간이 정해진 시간보다는 적으면서 0보다는 클경우.
+            return nowSecond - token.getCreateTokenTime() <= duration && nowSecond - token.getCreateTokenTime() >= 0;
+        }
+
+        return false;
+    }
+
+}
